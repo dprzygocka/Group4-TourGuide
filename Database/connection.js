717b39c8e0b895d7317db98b5cc12b1925be3773
Marianna <mana998@azet.sk>
2022-02-26 13:29:54 +0100

    Updated connection variables

--- Database/connection.js
+++ Database/connection.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
-const pool  = mysql.createPool(
-  process.env.CLEARDB_DATABASE_URL, {
+const pool  = mysql.createPool({
+  host     : process.env.HOST,
+  database : process.env.DATABASE,
+  user     : process.env.USER,
+  password : process.env.PASSWORD,
   connectionLimit : 5,
 });
 
