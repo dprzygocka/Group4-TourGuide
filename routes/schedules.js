2a02d9f127c3a2b3d7c588140ce546a466c75a2f
dprzygocka <przygocka.dagmara@gmail.com>
2022-02-28 14:46:18 +0100

    changes in schedule api set up

--- routes/schedules.js
+++ routes/schedules.js
@@ -4,13 +4,14 @@ const { Schedule } = require('../models/Schedule');
 
 router.get('/api/mysql/schedules', (req, res) => {
     pool.getConnection((err, db) => {
-        let query = 'SELECT * FROM schedule';
+        let query = 'SELECT schedule.*, guide.first_name, guide.last_name FROM schedule join guide on schedule.guide_id = guide.guide_id;';
         db.query(query, [], (error, result, fields) => {
             if (result && result.length) {
                 const schedules = [];
-                for (const tour of result) {
+                for (const schedule of result) {
+                    console.log(schedule);
                     //create new object
-                    schedules.push(new Schedule(schedule.schedule_id, schedule.no_free_spaces, schedule.date_time, schedule.tour_id, schedule.guide_id));
+                    schedules.push(new Schedule(schedule.schedule_id, schedule.no_free_spaces, schedule.date_time, schedule.tour_id, {guideId: schedule.guide_id, name: schedule.first_name, lastName: schedule.last_name} ));
                 }
                 res.send(schedules);
             } else {
@@ -25,10 +26,11 @@ router.get('/api/mysql/schedules', (req, res) => {
 
 router.get('/api/mysql/schedules/:schedule_id', (req, res) => {
     pool.getConnection((err, db) => {
-        let query = 'SELECT * FROM schedule WHERE schedule_id = ?';
+        let query = 'SELECT schedule.*, guide.first_name, guide.last_name FROM schedule join guide on schedule.guide_id = guide.guide_id WHERE schedule_id = ?';
         db.query(query, [req.params.schedule_id], (error, result, fields) => {
             if (result && result.length) {
-                res.send(new Schedule(result[0].schedule_id, result[0].no_free_spaces, result[0].date_time, result[0].tour_id, result[0].guide_id));
+                console.log(result);
+                res.send(new Schedule(result[0].schedule_id, result[0].no_free_spaces, result[0].date_time, result[0].tour_id, {guideId: result[0].guide_id, name: result[0].first_name, lastName: result[0].last_name}));
             } else {
                 res.send({
                     message: 'No results',
