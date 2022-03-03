2d3d00c669a64b434720c3576b33a0fdd88ef0e2
dprzygocka <przygocka.dagmara@gmail.com>
2022-03-03 11:27:35 +0100

    change naming

--- routes/bookings.js
+++ routes/bookings.js
@@ -12,7 +12,7 @@ router.get('/api/mysql/bookings', (req, res) => {
                 const bookings = [];
                 for (const booking of result) {
                     //create new object
-                    bookings.push(new Booking(booking.number_of_spots, booking.total_price, booking.date_time, new Customer(booking.customer_id, booking.first_name, booking.last_name), new Schedule(booking.schedule_id)));
+                    bookings.push(new Booking(booking.number_of_spots, booking.total_price, booking.booking_date_time, new Customer(booking.customer_id, booking.first_name, booking.last_name), new Schedule(booking.schedule_id)));
                 }
                 res.send(bookings);
             } else {
@@ -30,7 +30,7 @@ router.get('/api/mysql/bookings/:schedule_id/:customer_id', (req, res) => {
         let query = 'SELECT booking.*, customer.first_name, customer.last_name FROM booking join customer on booking.customer_id = customer.customer_id WHERE booking.schedule_id = ? AND booking.customer_id = ?;';
         db.query(query, [req.params.schedule_id, req.params.customer_id], (error, result, fields) => {
             if (result && result.length) {
-                res.send(new Booking(result[0].number_of_spots, result[0].total_price, result[0].date_time, new Customer(result[0].customer_id, result[0].first_name, result[0].last_name), new Schedule(result[0].schedule_id)));
+                res.send(new Booking(result[0].number_of_spots, result[0].total_price, result[0].booking_date_time, new Customer(result[0].customer_id, result[0].first_name, result[0].last_name), new Schedule(result[0].schedule_id)));
             } else {
                 res.send({
                     message: 'No results',
