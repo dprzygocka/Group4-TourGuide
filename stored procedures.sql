d157b57723e97316d9487c954b188f17cd868755
Marianna <mana998@azet.sk>
2022-03-07 13:21:19 +0100

    added remaining stored objects

--- stored procedures.sql
+++ stored procedures.sql
@@ -22,10 +22,10 @@ BEGIN
 END $$
 DELIMITER ;
 
-SELECT schedule.guide_id, ROUND(AVG(rating),2) AS average  FROM guide_rating JOIN schedule ON schedule.schedule_id = guide_rating.schedule_id WHERE guide_rating_id = 2;
+/*SELECT schedule.guide_id, ROUND(AVG(rating),2) AS average  FROM guide_rating JOIN schedule ON schedule.schedule_id = guide_rating.schedule_id WHERE guide_rating_id = 2;
 
 CALL CalculateRating(130, 'guide', @id, @average);
-CALL CalculateRating(130, 'guide');
+CALL CalculateRating(130, 'guide');*/
 
 DROP TRIGGER IF EXISTS update_guide_rating;
 DELIMITER $$
@@ -52,7 +52,7 @@ CREATE TRIGGER update_tour_rating
 	END $$
 DELIMITER ;
 
-
+/*
 SELECT guide_id FROM guide_rating JOIN schedule ON schedule.schedule_id = guide_rating.schedule_id WHERE guide_rating_id = 507;
 SELECT ROUND(AVG(guide_rating.rating),2) FROM guide_rating JOIN schedule ON schedule.schedule_id = guide_rating.schedule_id JOIN guide ON schedule.guide_id = guide.guide_id WHERE guide.guide_id = 130;
             
@@ -64,7 +64,7 @@ INSERT INTO guide_rating (schedule_id, customer_id, rating, comment) VALUES
 (284, 492,5, "amet, dapibus id, blandit at, nisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel nisl. Quisque fringilla euismod enim. Etiam gravida molestie arcu. Sed");
 SELECT guide_id FROM schedule WHERE schedule_id = 284;
 SELECT * FROM guide WHERE guide_id = 130;
-
+*/
 
 
 -- works booking total_price based on amount of people
@@ -77,7 +77,7 @@ CREATE TRIGGER update_total_price
 	END $$ 
 DELIMITER ;
 
-SELECT t.price FROM booking JOIN schedule ON schedule.schedule_id = 5 JOIN tour AS t ON schedule.tour_id = t.tour_id LIMIT 1;
+-- SELECT t.price FROM booking JOIN schedule ON schedule.schedule_id = 5 JOIN tour AS t ON schedule.tour_id = t.tour_id LIMIT 1;
 -- works
 DROP TRIGGER IF EXISTS update_number_of_spots;
 DELIMITER $$
@@ -158,12 +158,128 @@ CREATE OR REPLACE VIEW tour_place_schedule AS
         JOIN guide ON schedule.guide_id = guide.guide_id;
     
     
-SELECT * FROM invoices_with_balance;
+DROP PROCEDURE IF EXISTS PaginateSort;
+DELIMITER $$ 
+-- starts with page 0
+CREATE PROCEDURE PaginateSort(tablename CHAR(30), sortcolumn CHAR(20), direction CHAR(4), size INT, page INT)
+BEGIN
+	SET @sql = CONCAT('SELECT * FROM ', tablename, ' ORDER BY ', sortcolumn,' ', direction, ' LIMIT ', size, ' OFFSET ', page * size);
+    PREPARE stmt1 FROM @sql;
+    EXECUTE stmt1;
+END $$
+DELIMITER ;   
+
+CALL PaginateSort('customer', 'first_name', 'ASC', 5, 1);
+    
+DROP PROCEDURE IF EXISTS ScheduleBetweenDates;
+DELIMITER $$ 
+CREATE PROCEDURE ScheduleBetweenDates(IN start_date DATE, IN end_date DATE)
+BEGIN
+	-- SET start_date = str_to_date(start_date, '%YYYY-%MM-DD%');
+    -- SET end_date = str_to_date(end_date, '%YYYY-%MM-DD%');
+	SET @sql = CONCAT('SELECT schedule.*, guide.first_name, guide.last_name, tour.difficulty, tour.duration, tour.description, place.place_name FROM schedule join guide on schedule.guide_id = guide.guide_id join tour on schedule.tour_id = tour.tour_id join place on tour.place_of_destination_id = place.place_id WHERE schedule.schedule_date_time BETWEEN "', start_date, '" AND "', end_date,'"');
+    PREPARE stmt1 FROM @sql;
+    EXECUTE stmt1;
+END $$
+DELIMITER ;       
+
+DROP TRIGGER IF EXISTS check_booking_insert;
+DELIMITER $$
+CREATE TRIGGER check_booking_insert
+	BEFORE INSERT ON booking
+	FOR EACH ROW BEGIN
+		DECLARE start_date DATETIME;
+        SET start_date = (SELECT schedule_date_time FROM schedule WHERE schedule.schedule_id = NEW.schedule_id);
+		IF (start_date <= NEW.booking_date_time) THEN 
+			SIGNAL SQLSTATE '45000' 
+			SET MESSAGE_TEXT = "ERROR: You cannot book schedules from the past.";
+		END IF;
+	END $$
+DELIMITER ;
+
+INSERT INTO booking (number_of_spots, booking_date_time, customer_id, schedule_id) VALUES (5, "2024-4-16 4:40:34", 352, 1);
+
+-- works
+DROP TRIGGER IF EXISTS tour_change_is_active;
+DELIMITER $$
+CREATE TRIGGER tour_change_is_active
+	AFTER DELETE ON place
+	FOR EACH ROW BEGIN
+		UPDATE tour set is_active = 0 WHERE tour.place_of_departure_id = OLD.place_id AND tour.place_of_destination_id = OLD.place_id;
+	END $$ 
+DELIMITER ;
+-- delete from place where place_id = 255;
+
+-- works
+DROP TRIGGER IF EXISTS delete_guide_ratings;
+DELIMITER $$
+CREATE TRIGGER delete_guide_ratings
+	BEFORE DELETE ON guide
+	FOR EACH ROW BEGIN
+		DELETE FROM guide_rating WHERE guide_rating.schedule_id IN (SELECT schedule.schedule_id FROM schedule WHERE schedule.guide_id = OLD.guide_id);
+	END $$ 
+DELIMITER ;
+delete from guide where guide_id = 130;
+-- delete from gudie rating where guide is 130
+
+-- works
+DROP TRIGGER IF EXISTS delete_tour_ratings;
+DELIMITER $$
+CREATE TRIGGER delete_tour_ratings
+	BEFORE DELETE ON tour
+	FOR EACH ROW BEGIN
+		DELETE FROM tour_rating WHERE tour_rating.schedule_id IN (SELECT schedule.schedule_id FROM schedule WHERE schedule.tour_id = OLD.tour_id);
+	END $$ 
+DELIMITER ;
+    
+CREATE OR REPLACE VIEW tour_rating_view AS
+	SELECT place.place_name AS destination, tour.price, tour.description, tour.rating AS "full rating", 
+			schedule.schedule_date_time,
+            tour_rating.rating, tour_rating.comment
+    FROM tour 
+		JOIN place ON tour.place_of_departure_id = place.place_id
+        JOIN schedule ON tour.tour_id = schedule.tour_id
+        JOIN tour_rating ON schedule.schedule_id = tour_rating.schedule_id
+	ORDER BY tour.tour_id;
+ 
+SELECT * FROM tour_rating_view; 
+        
+CREATE OR REPLACE VIEW guide_rating_view AS
+	SELECT guide.first_name, guide.last_name, guide.rating AS "full rating",
+			place.place_name AS destination, tour.description, 
+			schedule.schedule_date_time,
+            guide_rating.rating, guide_rating.comment
+    FROM guide 
+        JOIN schedule ON guide.guide_id = schedule.guide_id
+		JOIN tour ON tour.tour_id = schedule.tour_id
+		JOIN place ON tour.place_of_departure_id = place.place_id
+        JOIN guide_rating ON schedule.schedule_id = guide_rating.schedule_id;
+        
+SELECT * FROM guide_rating_view; 
+    
+    
+    
+    
+-- SELECT * FROM invoices_with_balance;
 
 -- EXPLAIN SELECT*FROMcustomersWHEREstate='WV';
-EXPLAIN SELECT * FROM tour WHERE rating > 2; -- 500
+-- price, date, rating, password
+SELECT * FROM booking;
+SELECT * FROM tour WHERE rating > 2;
+EXPLAIN SELECT * FROM tour ORDER BY rating LIMIT 10; -- 500, 33.33,  500 100
+CREATE INDEX idx_rating ON tour (rating); -- 500, 32.40, 22, 100,   10, 100
+DROP INDEX idx_rating ON tour;
+ SELECT * FROM schedule WHERE schedule.schedule_date_time > "2017-11-9 9:12:24";
+EXPLAIN SELECT * FROM schedule WHERE schedule.schedule_date_time > "2017-11-9 9:12:24"; -- 500,33.33
+CREATE INDEX idx_date ON schedule (schedule_date_time); -- 500, 41.00
+EXPLAIN SELECT * FROM schedule WHERE schedule.schedule_date_time > "2017-11-9 9:12:24" ORDER BY schedule.schedule_date_time DESC;
+DROP INDEX idx_date ON schedule;
+EXPLAIN SELECT * FROM tour WHERE price < 50; -- 500 33.33
+CREATE INDEX idx_price ON tour (price); -- 13, 100
+
+EXPLAIN UPDATE tour SET rating = 5 WHERE tour_id = 5; -- 1 100
+
 EXPLAIN SELECT * FROM schedule JOIN tour ON schedule.tour_id = tour.tour_id ORDER BY rating;
 CREATE INDEX idx_rating ON tour (rating);
 EXPLAIN SELECT * FROM schedule JOIN tour ON schedule.tour_id = tour.tour_id JOIN place ON place.place_id = tour.place_of_destination_id ORDER BY place_id;
 EXPLAIN SELECT schedule.*, guide.first_name, guide.last_name, tour.difficulty, tour.duration, tour.description, place.place_name FROM schedule join guide on schedule.guide_id = guide.guide_id join tour on schedule.tour_id = tour.tour_id join place on tour.place_of_destination_id = place.place_id;
-EXPLAIN UPDATE tour SET rating = 5 WHERE tour_id = 5; -- 1 100
