4637792c0b7c378bddf768526773248effe04475
dprzygocka <przygocka.dagmara@gmail.com>
2022-04-04 11:20:37 +0200

    fix prifles

--- database_roles_and_profiles.sql
+++ database_roles_and_profiles.sql
@@ -1,27 +1,27 @@
-DROP ROLE 'tour_manager';
-DROP ROLE 'tour_developer';
-DROP ROLE 'tour_admin';
+DROP ROLE IF EXISTS 'tour_manager';
+DROP ROLE IF EXISTS 'tour_developer';
+DROP ROLE IF EXISTS 'tour_admin';
 
 CREATE ROLE 'tour_manager', 'tour_developer', 'tour_admin';
 
--- admin role --
-GRANT ALL ON tourguide.* TO 'admin';
+-- tour_admin role --
+GRANT ALL ON tourguide.* TO 'tour_admin';
 
--- developer role --
-GRANT SELECT ON tourguide.* TO 'developer';
-GRANT INSERT, UPDATE, DELETE ON tourguide.guide TO 'developer';
-GRANT INSERT, UPDATE, DELETE ON tourguide.place TO 'developer';
-GRANT INSERT, UPDATE, DELETE ON tourguide.schedule TO 'developer';
-GRANT INSERT, UPDATE, DELETE ON tourguide.tour TO 'developer';
-GRANT DELETE ON tourguide.guide_rating TO 'developer';
-GRANT DELETE ON tourguide.tour_rating TO 'developer';
-GRANT EXECUTE, ALTER ROUTINE ON PROCEDURE CalculateRating TO 'developer';
-GRANT EXECUTE, ALTER ROUTINE ON FUNCTION DeleteOldCustomers TO 'developer';
-GRANT EXECUTE, ALTER ROUTINE ON PROCEDURE PaginateSort TO 'developer';
-GRANT EXECUTE, ALTER ROUTINE ON PROCEDURE ScheduleBetweenDates TO 'developer';
-GRANT SELECT, ALTER ON tourguide.guide_rating_view TO 'developer';
-GRANT SELECT, ALTER ON tourguide.tour_place_schedule TO 'developer';
-GRANT SELECT, ALTER ON tourguide.tour_rating_view TO 'developer';
+-- tour_developer role --
+GRANT SELECT ON tourguide.* TO 'tour_developer';
+GRANT INSERT, UPDATE, DELETE ON tourguide.guide TO 'tour_developer';
+GRANT INSERT, UPDATE, DELETE ON tourguide.place TO 'tour_developer';
+GRANT INSERT, UPDATE, DELETE ON tourguide.schedule TO 'tour_developer';
+GRANT INSERT, UPDATE, DELETE ON tourguide.tour TO 'tour_developer';
+GRANT DELETE ON tourguide.guide_rating TO 'tour_developer';
+GRANT DELETE ON tourguide.tour_rating TO 'tour_developer';
+GRANT EXECUTE, ALTER ROUTINE ON PROCEDURE CalculateRating TO 'tour_developer';
+GRANT EXECUTE, ALTER ROUTINE ON FUNCTION DeleteOldCustomers TO 'tour_developer';
+GRANT EXECUTE, ALTER ROUTINE ON PROCEDURE PaginateSort TO 'tour_developer';
+GRANT EXECUTE, ALTER ROUTINE ON PROCEDURE ScheduleBetweenDates TO 'tour_developer';
+GRANT SELECT, ALTER ON tourguide.guide_rating_view TO 'tour_developer';
+GRANT SELECT, ALTER ON tourguide.tour_place_schedule TO 'tour_developer';
+GRANT SELECT, ALTER ON tourguide.tour_rating_view TO 'tour_developer';
 
 -- tour_manager role --
 GRANT SELECT ON tourguide.guide TO 'tour_manager';
@@ -44,5 +44,5 @@ CREATE USER 'first_developer' identified by 'passwordDeveloper';
 CREATE USER 'first_admin' identified by 'passwordAdmin';
 
 GRANT 'tour_manager' TO 'first_manager';
-GRANT 'developer' TO 'first_developer';
+GRANT 'tour_developer' TO 'first_developer';
 GRANT 'admin' TO 'first_admin';
