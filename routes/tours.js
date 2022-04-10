943be9e1f9c112ee981bdd4493cd6064d1416939
Marianna <mana998@azet.sk>
2022-04-10 21:16:39 +0200

    Refactored put tour to sequelize

--- routes/tours.js
+++ routes/tours.js
@@ -118,25 +118,30 @@ router.delete('/api/mysql/tours/:tour_id', async (req, res) => {
         res.send(error);
     }
 });
-/*
-router.put('/api/mysql/tours/:tour_id', (req, res) => {
-    pool.getConnection((err, db) => {
-        let query = 'UPDATE tour SET difficulty = ?, price = ?, duration = ?, number_of_spots = ?, age_limit = ?, distance = ?, description = ?, place_of_departure_id = ?, place_of_destination_id = ?, is_active = ? WHERE tour_id = ?;';
-        db.query(query, [req.body.difficulty, req.body.price, req.body.duration, req.body.numberOfSpots, req.body.ageLimit, req.body.distance, req.body.description, req.body.placeOfDeparture, req.body.placeOfDestination, req.body.isActive, req.params.tour_id], (error, result, fields) => {
-            if (result && result.affectedRows === 1) {
-                res.send({
-                    message: 'Tour successfully updated.',
-                });
-            } else {
-                res.send({
-                    message: 'Something went wrong',
-                });
-            }
-        });
-        db.release();
-    });
-});
 
+router.put('/api/mysql/tours/:tour_id', async (req, res) => {
+    try {
+        await Tour.update({
+            difficulty: req.body.difficulty,
+            price: req.body.price,
+            duration: req.body.duration,
+            numberOfSpots: req.body.numberOfSpots,
+            ageLimit: req.body.ageLimit,
+            distance: req.body.distance,
+            description: req.body.description,
+            placeOfDeparture: req.body.placeOfDeparture,
+            placeOfDestination: req.body.placeOfDestination
+        }, {
+            where: {
+                id: req.params.tour_id
+            }
+        })
+        res.send("Tour updated");
+    } catch (error) {
+        res.send(error);
+    }
+});
+/*
 //update one field
 router.patch('/api/mysql/tours/:tour_id', (req, res) => {
     const fieldName = req.body.fieldName;
