24951f8bce132f2180b75e2947ca08499edfbfb6
Marianna <mana998@azet.sk>
2022-04-10 20:31:58 +0200

    Set up sequelize

--- Database/connection.js
+++ Database/connection.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+const { Sequelize } = require('sequelize');
 
-const pool  = mysql.createPool({
-  host     : process.env.HOST,
-  database : process.env.DATABASE,
-  user     : process.env.USER,
-  password : process.env.PASSWORD,
-  connectionLimit : 5,
-});
+const pool = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD, {
+    pool: {
+      max: 5
+    },
+    host: process.env.HOST,
+    dialect: 'mysql'
+  }
+)
 
 module.exports = {
   pool,
