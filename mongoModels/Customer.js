e7ba800ce075189aba1da943cbfb6562f1b6305b
Marianna <mana998@azet.sk>
2022-04-15 18:34:32 +0200

    Added customer mongo model

---
+++ mongoModels/Customer.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        maxLength: 60
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxLength: 60
+    },
+    password: {
+        type: String,
+        required: true,
+        maxLength: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        maxLength: 127
+    },
+    phone: {
+        type: String,
+        required: true,
+        maxLength: 20
+    },
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
