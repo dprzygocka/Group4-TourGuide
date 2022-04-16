152d3b2aca6f6e236f3155689c240f1305537eb0
Marianna <mana998@azet.sk>
2022-04-16 14:29:35 +0200

    Fixed variable types

--- mongoModels/Customer.js
+++ mongoModels/Customer.js
@@ -2,28 +2,28 @@ const mongoose = require('mongoose');
 
 const customerSchema = new mongoose.Schema({
     firstName: {
-        type: String,
+        type: mongoose.String,
         required: true,
         maxLength: 60
     },
     lastName: {
-        type: String,
+        type: mongoose.String,
         required: true,
         maxLength: 60
     },
     password: {
-        type: String,
+        type: mongoose.String,
         required: true,
         maxLength: 100
     },
     email: {
-        type: String,
+        type: mongoose.String,
         required: true,
         maxLength: 127,
         unique: true
     },
     phone: {
-        type: String,
+        type: mongoose.String,
         required: true,
         maxLength: 20
     },
