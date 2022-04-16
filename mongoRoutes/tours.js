f32b42c59408f90609ae9d44a9e5fb242bc1048b
Marianna <mana998@azet.sk>
2022-04-16 14:55:08 +0200

    Added create tour for mongodb

---
+++ mongoRoutes/tours.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const Tour = require('../mongoModels/Tour.js');
+const mongodb = require('../Database/connection_mongo');
+const {checkDirection, checkSortColumn} = require('../models/Utils');
+
+router.post('/api/mongodb/tours', async(req, res) => {
+    try {
+        const tour = await Tour.create({
+            difficulty: req.body.difficulty,
+            price: req.body.price,
+            duration: req.body.duration,
+            number_of_spots: req.body.numberOfSpots,
+            age_limit: req.body.ageLimit,
+            distance: req.body.distance,
+            description: req.body.description,
+            place_of_departure_name: req.body.placeOfDeparture,
+            place_of_destination_name: req.body.placeOfDestination,
+            is_active: req.body.isActive
+        })
+        res.send(tour);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+
+
+module.exports = {
+  router,
+};
