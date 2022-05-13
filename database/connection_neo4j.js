e365d2d99ad1aab528842a642a0a482b2d1dc6c3
Marianna <mana998@azet.sk>
2022-05-13 13:45:53 +0200

    ADded models to neode instance

--- database/connection_neo4j.js
+++ database/connection_neo4j.js
@@ -2,7 +2,9 @@ const Neode = require('neode');
 require('dotenv').config();
 const router = require('express').Router();
 const instance = new Neode(process.env.NEO4J_URI, process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD, true).with({
-    Place: require('../neo4jModels/Place')
+    Place: require('../neo4jModels/Place'),
+    Tour: require('../neo4jModels/Tour'),
+    Schedule: require('../neo4jModels/Schedule'),
 });
 
 module.exports = { instance };
