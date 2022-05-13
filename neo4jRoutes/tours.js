1ece69df126b3978695327a310bf1ff489ee84dc
Marianna <mana998@azet.sk>
2022-05-13 13:44:58 +0200

    Added tours router with getall

---
+++ neo4jRoutes/tours.js
@@ -0,0 +1,18 @@
+const {instance} = require('../database/connection_neo4j');
+const router = require('express').Router();
+
+router.get('/api/neo4j/tours', async (req, res) => {
+    instance.all('Tour').then(res => {
+        return res.toJson();
+    })
+    .then(json => {
+        res.send(json);
+    })
+    .catch(e => {
+        res.status(500).send(e.stack);
+    });
+});
+
+module.exports = {
+    router,
+};
