4c81487d5b80424e921d119b69fdf5a861121123
Marianna <mana998@azet.sk>
2022-05-13 13:44:43 +0200

    Optimized return in place router

--- neo4jRoutes/places.js
+++ neo4jRoutes/places.js
@@ -2,10 +2,15 @@ const {instance} = require('../database/connection_neo4j');
 const router = require('express').Router();
 
 router.get('/api/neo4j/places', (req, res) => {
-        instance.all('Place')
-        .then(collection => {
-            res.send(collection.get(1).get('placeName'));
+    instance.all('Place').then(res => {
+        return res.toJson();
     })
+    .then(json => {
+        res.send(json);
+    })
+    .catch(e => {
+        res.status(500).send(e.stack);
+    });
 });
 
 module.exports = {
