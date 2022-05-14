3e4852e61ff830db173d4f8bb4f7f417689ac9e8
Marianna <mana998@azet.sk>
2022-05-14 18:57:49 +0200

    Added paging sorting to get all tours neo4j

--- neo4jRoutes/tours.js
+++ neo4jRoutes/tours.js
@@ -1,16 +1,27 @@
 const {instance} = require('../database/connection_neo4j');
 const router = require('express').Router();
+const {v4: uuidv4 } = require('uuid');
+const {checkDirection, checkSortColumn} = require('../models/Utils');
 
 router.get('/api/neo4j/tours', async (req, res) => {
-    instance.all('Tour').then(res => {
-        return res.toJson();
-    })
-    .then(json => {
-        res.send(json);
-    })
-    .catch(e => {
-        res.status(500).send(e.stack);
-    });
+    const sortColumn = req.query.sortColumn || 'placeId';
+    const direction = req.query.direction || 'ASC';
+    const size = Number(req.query.size) || 10;
+    //paging starts from 0
+    const page = Number(req.query.page - 1) || 0;
+    const skip = page * size;
+    const order = {[sortColumn]: direction};
+    if (checkSortColumn(sortColumn) && checkDirection(direction)) {
+        instance.all('Tour', {}, order, size, skip).then(res => {  
+            return res.toJson();
+        })
+        .then(json => {
+            res.send(json);
+        })
+        .catch(e => {
+            res.status(500).send(e.stack);
+        });
+    }
 });
 
 module.exports = {
