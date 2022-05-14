c33c56a020e5d614fe96cc40914808135a9d6f80
Marianna <mana998@azet.sk>
2022-05-14 17:10:44 +0200

    Added get by id for neo4j places

--- neo4jRoutes/places.js
+++ neo4jRoutes/places.js
@@ -1,5 +1,6 @@
 const {instance} = require('../database/connection_neo4j');
 const router = require('express').Router();
+const {v4: uuidv4 } = require('uuid');
 
 router.get('/api/neo4j/places', (req, res) => {
     instance.all('Place').then(res => {
@@ -12,6 +13,20 @@ router.get('/api/neo4j/places', (req, res) => {
         res.status(500).send(e.stack);
     });
 });
+
+router.get('/api/neo4j/places/:place_id', (req, res) => {
+    instance.find('Place', req.params.place_id).then(res => {
+        return res.toJson();
+    })
+    .then(json => {
+        res.send(json);
+    })
+    .catch(e => {
+        res.status(500).send(e.stack);
+    });
+});
+
+
 
 module.exports = {
     router,
