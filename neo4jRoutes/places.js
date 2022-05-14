a8d1aeb3948d8983ac5de7922512d3faef81244c
Marianna <mana998@azet.sk>
2022-05-14 17:37:55 +0200

    Added paging sorting to get all places

--- neo4jRoutes/places.js
+++ neo4jRoutes/places.js
@@ -1,17 +1,27 @@
 const {instance} = require('../database/connection_neo4j');
 const router = require('express').Router();
 const {v4: uuidv4 } = require('uuid');
+const {checkDirection, checkSortColumn} = require('../models/Utils');
 
 router.get('/api/neo4j/places', (req, res) => {
-    instance.all('Place').then(res => {
-        return res.toJson();
-    })
-    .then(json => {
-        res.send(json);
-    })
-    .catch(e => {
-        res.status(500).send(e.stack);
-    });
+    const sortColumn = req.query.sortColumn || 'placeId';
+    const direction = req.query.direction || 'ASC';
+    const size = Number(req.query.size) || 10;
+    //paging starts from 0
+    const page = Number(req.query.page - 1) || 0;
+    const skip = page * size;
+    const order = {[sortColumn]: direction};
+    if (checkSortColumn(sortColumn) && checkDirection(direction)) {
+        instance.all('Place', {}, order, size, skip).then(res => {
+            return res.toJson();
+        })
+        .then(json => {
+            res.send(json);
+        })
+        .catch(e => {
+            res.status(500).send(e.stack);
+        });
+    }
 });
 
 router.get('/api/neo4j/places/:place_id', (req, res) => {
