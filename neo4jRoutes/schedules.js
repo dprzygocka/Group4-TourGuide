6858be02628ec4edf7e539a2a6a352e5fbaffdcd
Marianna <mana998@azet.sk>
2022-05-14 19:51:28 +0200

    Added paginate sorting to get all schedules in neo4j

--- neo4jRoutes/schedules.js
+++ neo4jRoutes/schedules.js
@@ -1,17 +1,29 @@
 const {instance} = require('../database/connection_neo4j');
 const router = require('express').Router();
+const {v4: uuidv4 } = require('uuid');
+const {checkDirection, checkSortColumn} = require('../models/Utils');
 
 router.get('/api/neo4j/schedules', async (req, res) => {
-    instance.all('Schedule').then(res => {
-        return res.toJson();
-    })
-    .then(json => {
-        res.send(json);
-    })
-    .catch(e => {
-        res.status(500).send(e.stack);
-    });
+    const sortColumn = req.query.sortColumn || 'scheduleId';
+    const direction = req.query.direction || 'ASC';
+    const size = Number(req.query.size) || 10;
+    //paging starts from 0
+    const page = Number(req.query.page - 1) || 0;
+    const skip = page * size;
+    const order = {[sortColumn]: direction};
+    if (checkSortColumn(sortColumn) && checkDirection(direction)) {
+        instance.all('Schedule', {}, order, size, skip).then(res => {  
+            return res.toJson();
+        })
+        .then(json => {
+            res.send(json);
+        })
+        .catch(e => {
+            res.status(500).send(e.stack);
+        });
+    }
 });
+
 
 module.exports = {
     router,
