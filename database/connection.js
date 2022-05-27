556836c3393d4fde26677840b98cac9e7445272d
dprzygocka <przygocka.dagmara@gmail.com>
2022-05-27 17:35:09 +0200

    .env_helper and connection mysql updatew

--- database/connection.js
+++ database/connection.js
@@ -3,28 +3,19 @@ const mysql = require('mysql2');
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.USER,
-  process.env.PASSWORD, {
+  process.env.MYSQL_AZURE_DATABASE,
+  process.env.MYSQL_AZURE_USER,
+  process.env.MYSQL_AZURE_PASSWORD, {
     pool: {
       max: 5
     },
-    host: process.env.HOST,
+    host: process.env.MYSQL_AZURE_HOST,
     dialect: 'mysql',
     define: {
       timestamps: false
     }
   }
 )
-
-/*
-const pool  = mysql.createPool({
-  host     : process.env.HOST,
-  database : process.env.DATABASE,
-  user     : process.env.USER,
-  password : process.env.PASSWORD,
-  connectionLimit : 5,
-}); */
 
 const pool  = mysql.createPool({
   host     : process.env.MYSQL_AZURE_HOST,
